a45fa01458bd1898f5af8a357320204e77e693d0
PaStar98 <startekp98@gmail.com>
2024-11-03 19:25:02 +0100

    implement api feature

---
+++ src/api/IpDetailsApiResponse.ts
@@ -0,0 +1,18 @@
+interface IpDetailsApiResponse {
+  status: string;
+  country: string;
+  countryCode: string;
+  region: string;
+  regionName: string;
+  city: string;
+  zip: string;
+  lat: number;
+  lon: number;
+  timezone: string;
+  isp: string;
+  org: string;
+  as: string;
+  query: string;
+}
+
+export default IpDetailsApiResponse;
